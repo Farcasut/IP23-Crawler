ede4cbcca23115d0b3c0ae7d51a82119ba2ec9f1
ValeuDoamne <alexacosmin99@gmail.com>
2023-04-28 22:07:33 +0300

    set each spider to hold a requests.Session()

--- src/crawler/pipelines.py
+++ src/crawler/pipelines.py
@@ -1,8 +1,7 @@
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# useful for handling different item types with a single interface
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from crawler.items import Product
 from urllib.parse import urlparse
@@ -51,9 +50,7 @@ class DownloadImages:
                     'image/bmp': '.bmp',
                     'image/x-windows-bmp': '.bmp',
                     'image/webp': '.webp',
-                    'image/svg+xml': '.svg',
-                }
-        self.requests_session = requests.Session()
+                    'image/svg+xml': '.svg', }
         self.headers = {
                     'Connection': 'keep-alive',
                 }
@@ -63,7 +60,7 @@ class DownloadImages:
         downloaded_images = []
         for image in item['images']:
             if self.is_valid_url(image):
-                downloaded_images.append(self.download_image(image))
+                downloaded_images.append(self.download_image(spider, image))
         item['images'] = downloaded_images
         return item
 
@@ -74,22 +71,24 @@ class DownloadImages:
         except ValueError:
             return False
 
-    def download_image(self, image_url) -> str|None:
-        requested = self.requests_session.get(image_url, headers=self.headers) 
+    def download_image(self, spider, image_url) -> str|None:
+        if hasattr(spider, 'requests_session') == False:
+            spider.requests_session = requests.Session()
+        requested = spider.requests_session.get(image_url, headers=self.headers) 
 
         image_data = requested.content
         try:
             file_extension = self.mime_to_extension[requested.headers['Content-Type']] 
         except KeyError:
             return None
-        filename = hashlib.sha256(image_data[:256]).hexdigest()
+        filename = hashlib.sha256(image_data).hexdigest()
         full_filename = filename + file_extension
     
         try: 
             with open(self.path+'/'+full_filename, 'wb') as file:
                 file.write(image_data)
-        except error:
-            print(f"Error while trying file to write in filname {path}/{full_filename}: {error}")
+        except:
+            print(f"Error while trying file to write in filname {path}/{full_filename}")
 
         return full_filename
 
