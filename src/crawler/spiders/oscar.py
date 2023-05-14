3059f1ec3bc11abb4c570e12720bfd8a8fabc5b6
Catalin <cataling.780@gmail.com>
2023-05-14 19:51:30 +0300

    Crawler Oscar

---
+++ src/crawler/spiders/oscar.py
@@ -0,0 +1,31 @@
+import re
+import scrapy
+from itemloaders import ItemLoader
+from scrapy import Selector
+from ..items import Product
+
+
+class Oscar(scrapy.Spider):
+    name = 'Oscar'
+    start_urls = ['https://restaurant-oscar.ro/']
+
+    def parse(self, response):
+        categories_pages = response.css('.wp-block-kadence-column .wp-block-kadence-infobox .kt-blocks-info-box-link-wrap::attr(href)').getall()
+        for category_page in categories_pages:
+            yield response.follow(category_page, callback= self.scrape_page)
+
+    def scrape_page(self, response):
+        products = response.css('.woocommerce-loop-product__title a::attr(href)').getall()
+        for product in products:
+            yield response.follow(product, callback=self.scrape_item)
+
+    def scrape_item(self, response,):
+        l = ItemLoader(item = Product(), selector= response)
+        l.add_value('restaurant_name',Oscar.name)
+        l.add_css('name', '.entry-title::text')
+        l.add_value('source', 'site')
+        l.add_css('price', '.price bdi::text')
+        l.add_css('images', '.product_image a::attr(href)')
+        l.add_css('category', '.single-product-category a::text')
+        l.add_css('description', '#tab-description p::text')
+        yield l.load_item()
