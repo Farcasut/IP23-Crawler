28393d0cea4b7d96f2182c4b08d71ce8b9e08f13
Farcasi Alexandru <farcasialexandrububu@gmail.com>
2023-05-14 21:15:42 +0300

    solved bug

--- src/crawler/spiders/blueaquca.py
+++ src/crawler/spiders/blueaquca.py
@@ -23,7 +23,10 @@ class BlueAcqua(scrapy.Spider):
         l.add_css('name', '.info h2::text')
         l.add_css('source', 'site')
         l.add_css('price', '.price::text'),
-        l.add_value('images', r"https://www.blueacqua.ro/"+ response.css('a::attr(href)').get())
+
+        image=  r"https://www.blueacqua.ro/"+ response.css('a::attr(href)').get();
+        if image is not None:
+            l.add_value('images',image )
         l.add_value('category', '')
         l.add_value('description', '')
         yield l.load_item()
